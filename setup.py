c7c7c0e909046d3820f315c0b169c6c7916a5661
Rick Copeland <rick@arborian.com>
2012-04-28 19:11:51 -0400

    Add monq command-line script to run monq tasks
    Signed-off-by: Rick Copeland <rick@arborian.com>

--- setup.py
+++ setup.py
@@ -1,25 +1,39 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.0'
+version = '0.1'
 
 setup(name='MonQ',
       version=version,
-      description="",
-      long_description="""\
-""",
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      keywords='',
-      author='',
-      author_email='',
-      url='',
-      license='',
+      description="Simple queue package for MongoDB",
+      long_description="""MonQ provides a simple queueing solution for MongoDB
+      for when you don't want to bother with Redis, Celery, or other solutions.
+     """,
+      classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+        ],
+      keywords='mongodb, queue',
+      author='Rick Copeland',
+      author_email='rick@arborian.com',
+      url='http://blog.pythonisito.com',
+      license='MIT',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
       zip_safe=False,
       install_requires=[
         'Ming',
+        'configglue',
       ],
+      scripts=[
+        'scripts/monq'
+        ],
       entry_points="""
       # -*- Entry points: -*-
       """,
